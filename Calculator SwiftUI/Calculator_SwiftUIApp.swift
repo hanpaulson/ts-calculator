017856910e249e25e4d64c6c5101c8f2c15067a9
Hannah <hannah.paulson@tigerspike.com>
2021-01-14 09:05:46 +0000

    first commit

---
+++ Calculator SwiftUI/Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_SwiftUIApp.swift
+//  Calculator SwiftUI
+//
+//  Created by Hannah Paulson on 14/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
