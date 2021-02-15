d30cec6eb78a551557353b446f22c77ceb1026e6
Hannah <hannah.paulson@tigerspike.com>
2021-02-15 13:45:11 +0000

    removed old steps

--- Calculator SwiftUIUITests/steps/CalculatorSteps.swift
+++ Calculator SwiftUIUITests/steps/CalculatorSteps.swift
@@ -12,80 +12,8 @@ class CalculatorSteps: BaseClass {
 
     let calculatorScreen = CalculatorScreen()
 
-    func divisionByZero() {
-        calculatorScreen.tapButton(.one)
-        calculatorScreen.tapButton(.division)
-        calculatorScreen.tapButton(.zero)
-        calculatorScreen.tapButton(.equal)
-    }
-
-    func divideZeroByZero() {
-        calculatorScreen.tapButton(.zero)
-        calculatorScreen.tapButton(.division)
-        calculatorScreen.tapButton(.zero)
-        calculatorScreen.tapButton(.equal)
-    }
-
-    func sumOperation() {
-        calculatorScreen.tapButton(.two)
-        calculatorScreen.tapButton(.addition)
-        calculatorScreen.tapButton(.five)
-        calculatorScreen.tapButton(.equal)
-    }
-
-    func subtractionOperationWithPositiveResult() {
-        calculatorScreen.tapButton(.six)
-        calculatorScreen.tapButton(.subtraction)
-        calculatorScreen.tapButton(.five)
-        calculatorScreen.tapButton(.equal)
-    }
-
-    func subtractionOperationWithNegativeResult() {
-        calculatorScreen.tapButton(.three)
-        calculatorScreen.tapButton(.subtraction)
-        calculatorScreen.tapButton(.nine)
-        calculatorScreen.tapButton(.equal)
-    }
-
-    func subtractionOperationStartingWithNegativeNumber() {
-        calculatorScreen.tapButton(.subtraction)
-        calculatorScreen.tapButton(.eight)
-        calculatorScreen.tapButton(.subtraction)
-        calculatorScreen.tapButton(.six)
-        calculatorScreen.tapButton(.equal)
-    }
-
-    func muliplicationOperation() {
-        calculatorScreen.tapButton(.three)
-        calculatorScreen.tapButton(.multiplication)
-        calculatorScreen.tapButton(.nine)
-        calculatorScreen.tapButton(.equal)
-    }
-
-    func decimalOperation() {
-        calculatorScreen.tapButton(.five)
-        calculatorScreen.tapButton(.point)
-        calculatorScreen.tapButton(.five)
-        calculatorScreen.tapButton(.subtraction)
-        calculatorScreen.tapButton(.one)
-        calculatorScreen.tapButton(.point)
-        calculatorScreen.tapButton(.one)
-        calculatorScreen.tapButton(.equal)
-    }
-
-    func numberWithTwoDecimalsOperation() {
-        calculatorScreen.tapButton(.eight)
-        calculatorScreen.tapButton(.point)
-        calculatorScreen.tapButton(.six)
-        calculatorScreen.tapButton(.six)
-        calculatorScreen.tapButton(.subtraction)
-        calculatorScreen.tapButton(.two)
-        calculatorScreen.tapButton(.point)
-        calculatorScreen.tapButton(.one)
-        calculatorScreen.tapButton(.one)
-        calculatorScreen.tapButton(.equal)
-    }
-
+    // Add steps here 
+    
     func validateCalculatorResult(_ text: String) {
         XCTAssertEqual(calculatorScreen.getOperationResult, text)
     }
---
+++ Calculator SwiftUIUITests/tests/Calculator_SwiftUIUITests.swift
@@ -0,0 +1,15 @@
+//
+//  Calculator_SwiftUIUITests.swift
+//  Calculator SwiftUIUITests
+//
+//  Created by Hannah Paulson on 14/01/2021.
+//
+
+import XCTest
+
+class Calculator_SwiftUIUITests: BaseClass {
+
+    let calculatorSteps = CalculatorSteps()
+
+   // Add tests here 
+}
